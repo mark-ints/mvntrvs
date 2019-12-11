2b3514206b5e9201833e05ea8d77c1619e2b7bf2
Mark Khazin <mark.khazin@intsights.com>
2019-12-11 15:36:24 +0200

    all commit

---
+++ src/main/java/org/testing/plugin/analysis/ExamplePlugin.java
@@ -0,0 +1,8 @@
+package org.testing.plugin.analysis;
+
+import org.elasticsearch.plugins.AnalysisPlugin;
+import org.elasticsearch.plugins.Plugin;
+
+public class ExamplePlugin extends Plugin implements AnalysisPlugin {
+}
+
